490e41cc6109317e988131b96d422d3db5fd495b
alexsantosfilho <devalexsandro@gmail.com>
2018-03-06 11:09:32 -0300

    Primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { Cursos2Component } from './cursos2/cursos2.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Cursos2Component
   ],
   imports: [
     BrowserModule
